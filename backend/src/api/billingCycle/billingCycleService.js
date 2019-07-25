f2d98942748e70726207385ad4d61dff5e144d30
Eduardo Pedrosa <dudupedrosao@gmail.com>
2019-07-25 09:47:07 -0300

    Dashboard finalizado

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -2,7 +2,7 @@ const BillingCycle = require('./billingCycle')
 const errorHandler = require('../common/errorHandler')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
-BillingCycle.updateOptions({ new: true, runValidators: true})
+BillingCycle.updateOptions({new: true, runValidators: true})
 BillingCycle.after('post', errorHandler).after('put', errorHandler)
 
 BillingCycle.route('count', (req, res, next) => {
@@ -22,11 +22,11 @@ BillingCycle.route('summary', (req, res, next) => {
         $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
     }, {
         $project: {_id: 0, credit: 1, debt: 1}
-    }], (error, result) => {
+    }]).exec (function (error, result){
         if(error) {
             res.status(500).json({errors: [error]})
         } else {
-            res.json(result[0] || {credit: 0, debt: 0})
+            res.json(result[0] || { credit: 0, debt: 0 })
         }
     })
 })
