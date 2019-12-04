6731662fd09566701fb010d35aee3a96ce763026
Eduardo Pedrosa <dudupedrosao@gmail.com>
2019-12-04 16:43:31 -0300

    removendo footer

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 
 import Header from '../common/template/header'
 import Sidebar from '../common/template/sidebar'
-import Footer from '../common/template/footer'
 import Routes from './routes'
 import Messages from '../common/msg/messages'
 
@@ -14,7 +13,6 @@ export default props => (
         <div className='content-wrapper'>
             <Routes />
         </div>
-        <Footer />
         <Messages />
     </div>
 )
